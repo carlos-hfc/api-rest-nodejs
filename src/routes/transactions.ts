c8aef058c867ec9d596e9ead166bbf32d1d6e8cc
Carlos Faustino <chfcchfc96@gmail.com>
2024-05-08 16:59:29 -0300

    feat: add list transactions and get by id

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -6,6 +6,30 @@ import { z } from "zod"
 import { knex } from "../database"
 
 export async function transactionsRoutes(app: FastifyInstance) {
+  app.get("/", async () => {
+    const transactions = await knex("transactions").select()
+
+    return { transactions }
+  })
+
+  app.get("/:id", async (request, reply) => {
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = paramsSchema.parse(request.params)
+
+    const transaction = await knex("transactions").where("id", id).first()
+
+    if (!transaction) {
+      return reply.code(404).send({
+        message: "Transaction not found.",
+      })
+    }
+
+    return { transaction }
+  })
+
   app.post("/", async (request, reply) => {
     const bodySchema = z.object({
       title: z.string(),
